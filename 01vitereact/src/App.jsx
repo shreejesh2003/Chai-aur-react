2c73e54499c5818861fbd773ff224c933e4fba02
shreejesh2003 <tshreejesh@gmail.com>
2024-08-12 18:51:29 +0530

    Basic react using vite

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Chai from './chai'
+
+
+function App() {
+  const username="chai aur code"
+
+  return (
+   <>
+   <h1>chai username:{username}</h1>
+   <Chai /></>
+  )
+}
+
+export default App
